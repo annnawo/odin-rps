22f5a4380e046f06fabe49a507d7e0d3bf28a69d
anna woodward <annagracewoodward.com>
2023-02-11 13:07:11 -0600

    Create player selection function

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ function getComputerChoice() {
 	let computerChoice;
 	if (randomNumber <= (1/3)) { 
 		computerChoice = 0;
-	}
-	else if (randomNumber <= (2/3)) {
+	} else if (randomNumber <= (2/3)) {
 		computerChoice = 1;
-	}
-	else {
+	} else {
 		computerChoice = 2;
 	}
 	return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playerSelection() {
+	let playerChoice = prompt('Please enter your desired move');
+	let playerSelection;
+	playerChoice = playerChoice.toLowerCase();
+	while (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
+		playerChoice = prompt('Try again. Please enter desired move');
+	 	playerChoice = playerChoice.toLowerCase();
+	 }
+	if (playerChoice === 'rock') {
+		playerSelection = 0;
+	} else if (playerChoice === 'paper') {
+		playerSelection = 1;
+	} else {
+		playerSelection = 2;
+	 }
+	 return playerSelection;
+}
+
+console.log(playerSelection());
